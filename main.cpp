f728f9f52ceb2d4381602f695fe2fa2fca9bdd81
Thomas-dev-cell <156655091+Thomas-dev-cell@users.noreply.github.com>
2024-12-29 00:05:56 +0100

    Display update main.cpp
    Updated Display. Tested. Still to be done : Explicit the method used (BS or MC); explicit hedge-cost ; manage bad input (if, in Menu, non int is entered, program will loop indefinitely (ie crash)) I will try to tackle these 3 problems tomorrow morning. 
    
    Thomas CHAIGNE

--- main.cpp
+++ main.cpp
@@ -8,87 +8,123 @@
 #include <vector>
 
 void displayMenu() {
-    std::cout << "\n--- Menu des options disponibles ---\n";
-    std::cout << "1. Call Option\n";
-    std::cout << "2. Put Option\n";
-    std::cout << "3. Barrier Option (Up-and-Out)\n";
-    std::cout << "4. Barrier Option (Down-and-Out)\n";
-    std::cout << "5. Barrier Option (Up-and-In)\n";
-    std::cout << "6. Barrier Option (Down-and-In)\n";
-    std::cout << "7. Asian Option (Call)\n";
-    std::cout << "8. Asian Option (Put)\n";
-    std::cout << "0. Quitter\n";
+    std::cout << "\n--- Menu ---\n\n";
+    std::cout << "0 - End Programm\n";
+    std::cout << '\n';
+    std::cout << "European Option\n";
+    std::cout << "---------------\n";
+    std::cout << "1 - Call\n";
+    std::cout << "2 - Put\n";
+    std::cout << '\n';
+    std::cout << "Barrrier Option\n";
+    std::cout << "---------------\n";
+    std::cout << "3 - Up-and-Out\n";
+    std::cout << "4 - Down-and-Out\n";
+    std::cout << "5 - Up-and-In\n";
+    std::cout << "6 - Down-and-In\n";
+    std::cout << '\n';
+    std::cout << "Asian Option\n";
+    std::cout << "------------\n";
+    std::cout << "7 - Call\n";
+    std::cout << "8 - Put\n";
+    std::cout << '\n';
 }
 
 int main() {
-    // Modèle de base Black-Scholes
-    double spot, rate, volatility, dividend;
-    std::cout << "Entrez les paramètres du modèle Black-Scholes :\n";
-    std::cout << "Spot price (S0) : ";
-    std::cin >> spot;
-    std::cout << "Risk-free rate (r) : ";
-    std::cin >> rate;
-    std::cout << "Volatility (sigma) : ";
-    std::cin >> volatility;
-    std::cout << "Dividend yield (q) : ";
-    std::cin >> dividend;
 
-    BlackScholesModel model(spot, rate, volatility, dividend);
+    double spot, rate, volatility, dividend;
+    std::cout << '\n';
+    std::cout << "OPTION PRICER\n";
+    std::cout << "-------------\n\n";
+    std ::cout<< "MARI M.\nDONATI ARRANZ M.\nCHAIGNE T.\n";
+
 
     while (true) {
+
+        // Parametres generaux
+
+        std::cout << "\n*************************";
+        std::cout << "\n\n";
+        std::cout << "--- Option Parameters ---\n";
+        std::cout << "\nSpot price\n    S0 = ";
+        std::cin >> spot;
+        std::cout << "\nRisk-free rate\n    r = ";
+        std::cin >> rate;
+        std::cout << "\nVolatility\n    sigma = ";
+        std::cin >> volatility;
+        std::cout << "\nDividend yield\n    q = ";
+        std::cin >> dividend;
+
+        BlackScholesModel model(spot, rate, volatility, dividend);
+
+        // Choix du type d'option 
+
         displayMenu();
-        int choice;
-        std::cout << "Choisissez une option : ";
-        std::cin >> choice;
+        
+        int choice{-1};
+        std::cout << "Choose option type: ";
+        std::cin >> choice; 
 
         if (choice == 0) {
-            std::cout << "Merci d'avoir utilisé le programme.\n";
+            std::cout << "\nEND\n";
             break;
         }
 
-        // Paramètres génériques pour toutes les options
+        // Parametres generaux (suite)
+
         double strike, maturity;
-        std::cout << "Entrez le strike (K) : ";
+        std::cout << '\n';
+        std::cout << "Strike price\n    K = ";
         std::cin >> strike;
-        std::cout << "Entrez la maturité (T, en années) : ";
+        std::cout << '\n';
+        std::cout << "Maturity in years\n    T = ";
         std::cin >> maturity;
+
+
+        // Resultats
 
         int numPaths = 10000;  // Nombre de simulations Monte-Carlo
         int steps = 100;       // Nombre de pas temporels
 
         if (choice == 1) {
             // Call Option
+            std::cout << "\nComputing..." << std::flush;
             CallOption callOption(strike, maturity);
             double price = model.priceAnalytic(&callOption, true);
             double hedgeCost = callOption.hedgeCost(model, steps);
-            std::cout << "Prix du call option : " << price << "\n";
-            std::cout << "Coût de réplication : " << hedgeCost << "\n";
+            std::cout << "\n\n--- Results ---\n\n";
+            std::cout << "Option price..........." << price << "\n";
+            std::cout << "Hedge cost............." << hedgeCost << "\n";
 
         } else if (choice == 2) {
             // Put Option
+            std::cout << "\nComputing..." << std::flush;
             PutOption putOption(strike, maturity);
             double price = model.priceAnalytic(&putOption, false);
             double hedgeCost = putOption.hedgeCost(model, steps);
-            std::cout << "Prix du put option : " << price << "\n";
-            std::cout << "Coût de réplication : " << hedgeCost << "\n";
+            std::cout << "\n\n--- Results ---\n\n";
+            std::cout << "Option price..........." << price << "\n";
+            std::cout << "Hedge cost............." << hedgeCost << "\n";
 
         } else if (choice >= 3 && choice <= 6) {
             // Barrier Options
             double barrier;
-            std::cout << "Entrez le niveau de la barrière (B) : ";
+            std::cout << '\n';
+            std::cout << "Barrier level:\n    B = ";
             std::cin >> barrier;
 
             // Demander le type de l'option (Call ou Put)
             OptionType optionType;
             int optionTypeChoice;
-            std::cout << "Type d'option : 1 pour Call, 2 pour Put : ";
+            std::cout << "\nOption type ? Call = 1 ; Put = 2\n";
             std::cin >> optionTypeChoice;
+            std::cout << "\nComputing..." << std::flush;
             if (optionTypeChoice == 1) {
                 optionType = OptionType::Call;
             } else if (optionTypeChoice == 2) {
                 optionType = OptionType::Put;
             } else {
-                std::cout << "Choix invalide pour le type d'option.\n";
+                std::cout << "Invalid input\n";
                 continue;
             }
 
@@ -111,21 +147,24 @@ int main() {
             BarrierOption barrierOption(strike, maturity, barrier, barrierType, optionType);
             double price = barrierOption.price(model, numPaths, steps);
             double hedgeCost = barrierOption.hedgeCost(model,steps);
-            std::cout << "Prix de l'option barrière : " << price << "\n";
-            std::cout << "Coût de réplication : " << hedgeCost << "\n";
+            std::cout << "\n\n--- Results ---\n\n";
+            std::cout << "Option price..........." << price << "\n";
+            std::cout << "Hedge cost............." << hedgeCost << "\n";
 
         } else if (choice == 7 || choice == 8) {
             // Asian Options
+            std::cout << "\nComputing..." << std::flush;
             OptionType optionType = (choice == 7) ? OptionType::Call : OptionType::Put;
 
             AsianOption asianOption(strike, maturity, optionType);
             double price = asianOption.price(model, numPaths, steps);
             double hedgeCost = asianOption.hedgeCost(model, steps);
-            std::cout << "Prix de l'option asiatique : " << price << "\n";
-            std::cout << "Coût de réplication : " << hedgeCost << "\n";
+            std::cout << "\n\n--- Results ---\n\n";
+            std::cout << "Option price..........." << price << "\n";
+            std::cout << "Hedge cost............." << hedgeCost << "\n";
 
         } else {
-            std::cout << "Choix invalide. Veuillez réessayer.\n";
+            std::cout << "Invalid input\n";
         }
     }
 
